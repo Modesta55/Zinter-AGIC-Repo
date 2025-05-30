f6123c5d3bf81098d9e52c74a79b0c612bd0feb6
Modesta55 <uchechimodesta97@gmail.com>
2025-05-30 14:45:50 +0100

    new commit

---
+++ terraform/app_gateway/main.tf
@@ -0,0 +1,84 @@
+provider "azurerm" {
+  features {}
+}
+
+# Create public IP for App Gateway
+resource "azurerm_public_ip" "appgw_ip" {
+  name                = "appgw-public-ip"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+
+# Create virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "appgw-vnet"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Create subnet for App Gateway
+resource "azurerm_subnet" "appgw_subnet" {
+  name                 = "appgw-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/27"]
+}
+
+# Create App Gateway
+resource "azurerm_application_gateway" "appgw" {
+  name                = var.app_gateway_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  sku {
+    name     = "WAF_v2"
+    tier     = "WAF_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "appgw-ip-config"
+    subnet_id = azurerm_subnet.appgw_subnet.id
+  }
+
+  frontend_port {
+    name = "frontend-port"
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = "frontend-ip"
+    public_ip_address_id = azurerm_public_ip.appgw_ip.id
+  }
+
+  backend_address_pool {
+    name = "default-backend"
+  }
+
+  backend_http_settings {
+    name                  = "default-settings"
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 30
+  }
+
+  http_listener {
+    name                           = "listener"
+    frontend_ip_configuration_name = "frontend-ip"
+    frontend_port_name             = "frontend-port"
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = "rule1"
+    rule_type                  = "Basic"
+    http_listener_name         = "listener"
+    backend_address_pool_name  = "default-backend"
+    backend_http_settings_name = "default-settings"
+  }
+}
---
+++ terraform/app_gateway/variables.tf
@@ -0,0 +1,16 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region (e.g., eastus)"
+}
+
+
+
+variable "app_gateway_name" {
+  type        = string
+  description = "Name of the Application Gateway"
+}
